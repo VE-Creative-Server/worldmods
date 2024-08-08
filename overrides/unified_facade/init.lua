92cfdff8b5d1fe0509c4e38a1c9e15a10d80ad24
wsor4035 <24964441+wsor4035@users.noreply.github.com>
2024-08-08 20:15:27 +0000

    updated unified_facade

--- overrides/unified_facade/init.lua
+++ overrides/unified_facade/init.lua
@@ -1,14 +1,34 @@
-local facade_items = {}
 
-for name, def in pairs(minetest.registered_nodes) do
-    if string.match(name, "^facade:") then
-        table.insert(facade_items, name)
-    end
+if minetest.get_modpath("facade") then
+  local facade_items = {}
+
+  for name, def in pairs(minetest.registered_nodes) do
+     if string.match(name, "^facade:") then
+          table.insert(facade_items, name)
+      end
+  end
+
+  unified_inventory.register_category("facade", {
+   symbol = "facade:shaper",
+   label = "facade bannerstones",
+   index = 0,
+   items = facade_items,
+  })
 end
 
-unified_inventory.register_category("facade", {
-  symbol = "facade:shaper",
-  label = "facade bannerstones",
-  index = 5,
-  items = facade_items,
-})
+if minetest.get_modpath("gates") then
+ local nodename = "gates:controler"
+ local node = table.copy(minetest.registered_nodes[nodename]) or {}
+ node.groups.not_in_creative_inventory = 1
+ node.on_place = function (itemstack, player, pointed_thing)
+   if minetest.get_player_privs(player:get_player_name()).worldedit then
+     return minetest.item_place(itemstack, player, pointed_thing)
+   else
+     return itemstack
+   end
+  end
+  minetest.override_item(nodename, {
+    groups = node.groups,
+    on_place = node.on_place,
+  })
+end
