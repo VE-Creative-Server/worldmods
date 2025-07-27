642490f83f0c9a20db617a46cf935bd9c5b4695d
wsor4035 <24964441+wsor4035@users.noreply.github.com>
2025-07-27 09:33:10 +0000

    added shutdown override

---
+++ overrides/shutdown/init.lua
@@ -0,0 +1,15 @@
+-- Prevent Admins shutting down server accidentally with players online.
+
+local old_shutdown = core.registered_chatcommands["shutdown"].func
+
+local function new_shutdown(name, param)
+  if (not param or param == "") and # core.get_connected_players() > 0 then
+    core.chat_send_all(core.colorize("red", "|#Server| * " .. name .. " attempted to shutdown server but forgot -r"))
+    return false, "There are players online!  please use full command with -r if you're sure about this."
+  end
+  
+  old_shutdown(name, param)
+  return true
+end
+
+core.override_chatcommand("shutdown", {func = new_shutdown})
