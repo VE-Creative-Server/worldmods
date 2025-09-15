302a726bc11258305c45771722427b6feea5857a
wsor4035 <24964441+wsor4035@users.noreply.github.com>
2025-09-15 14:54:00 +0000

    shutdown

--- overrides/shutdown/init.lua
+++ overrides/shutdown/init.lua
@@ -3,7 +3,7 @@
 local old_shutdown = core.registered_chatcommands["shutdown"].func
 
 local function new_shutdown(name, param)
-  if (not param or param == "") and # core.get_connected_players() > 0 then
+  if name ~= "cheapie" and (not param or param == "") and # core.get_connected_players() > 0 then
     core.chat_send_all(core.colorize("red", "|#Server| * " .. name .. " attempted to shutdown server but forgot -r"))
     return false, "There are players online!  please use full command with -r if you're sure about this."
   end
